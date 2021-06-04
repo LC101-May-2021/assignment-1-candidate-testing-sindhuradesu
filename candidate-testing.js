3b37182aa7694d96b7829c813449def11eb7035b
Sindhudesu <>
2021-06-04 13:48:40 +0000

    candidate testing Assaignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,42 +3,57 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName ="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question="Who was the first American woman in space? ";
+let correctAnswer ="Sally Ride";
+let candidateAnswer ="";
+let questions=['Who was the first American woman in space?','True or false: 5 kilometer == 5000 meters?','(5 + 3)/2 * 10 = ?',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",'What is the minimum crew size for the ISS?'];
+let correctAnswers =["Sally Ride","true","40","Trajectory","3"];
+let candidateAnswers=[];
+//let gradeQuiz;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+candidateName =input.question("Enter the Candidate Name: ");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  for(let i=0;i<questions.length;i++)
+  {
+    candidateAnswer =input.question(questions[i]);
+    candidateAnswers.push(candidateAnswer);
+  }
 
-
+//console.log("Your Answer is" ,candidateAnswer);
+//console.log("Correct Answer is",correctAnswer);
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
+for(let i=0;i<correctAnswers.length;i++)
+{
+if (candidateAnswers[i].toUpperCase()===correctAnswers[i].toUpperCase())
+{
+  console.log("Correct Answer");
+} else{
+ console.log("you gave Wrong Answer");
+}
+}
   let grade;
   
 
   return grade;
 }
 
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+console.log("Hello "+candidateName);  
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
